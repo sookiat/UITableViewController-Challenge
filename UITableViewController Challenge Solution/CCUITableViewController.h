c258958549a9b8750129e04815488b7f4fb19a3d
Soo Kiat Ter <lucastersk@gmail.com>
2014-04-04 05:56:00 +0100

    added CCUITableViewController.h .m

---
+++ UITableViewController Challenge Solution/CCUITableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCUITableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Soo Kiat Ter on 04/04/2014.
+//  Copyright (c) 2014 Can Can Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCUITableViewController : UITableViewController
+
+@end
